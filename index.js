258f0ed0a938e164a4649f5eb542d65567be44f0
SATYAMDCS <dubeysatyam3008@gmail.com>
2022-10-25 16:37:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const dotenv = require("dotenv"); //require dotenv package
+dotenv.config({ path: "./.env" }); //import config.env file
+
+
+const passport = require("passport");
+const passportLocal = require("./config/passport-local");
+const MongoStore = require("connect-mongo");
+
+const db = require("./config/mongoose");
+
+// const DB = process.env.DATABASE;
+const PORT = process.env.PORT;
+
+const app = express();
+
+// for static files
+app.use(expressLayouts);
+app.use(express.static("./assets"));
+// view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+//------BodyParser--------//
+app.use(express.urlencoded({ extended: false }));
+
+// to render css file link in header
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
+
+// middleware for use session cookie
+app.use(
+  session({
+    name: "employeeReview",
+    secret: process.env.SECRET,
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 1000 * 60 * 100,
+    },
+    store: MongoStore.create(
+      {
+        mongoUrl: "mongodb://localhost:27017/habit",
+        autoRemove: "disabled",
+      },
+      function (err) {
+        console.log(err || "connect-mongodb setup");
+      }
+    ),
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+
+app.use("/", require("./routes/index"));
+
+app.listen(process.env.PORT, function (err) {
+  if (err) {
+    console.log("Error while connecting to server");
+    return;
+  }
+  console.log(`Server running on port ${process.env.PORT}.`);
+});
