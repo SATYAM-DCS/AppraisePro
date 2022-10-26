3d40d8c43cecd0833e4b197c862d05731d7bc7d5
SATYAMDCS <dubeysatyam3008@gmail.com>
2022-10-26 16:24:27 +0530

    fianl2

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,6 @@
-const mongoose = require('mongoose');
 require('dotenv').config()
+const mongoose = require('mongoose');
+
 // connect from mongodb
  const dataBASE = process.env.DATABASE;
 mongoose.connect(dataBASE,
