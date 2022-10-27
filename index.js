2463bd7c82ec085fbc18f45bce6e186403f35dea
SATYAMDCS <dubeysatyam3008@gmail.com>
2022-10-27 14:49:06 +0530

    final with remove default at mongoStore

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const mongoose = require("mongoose");
 
 const passport = require("passport");
 const passportLocal = require("./config/passport-local");
-const MongoStore = require("connect-mongo").session;
+const MongoStore = require("connect-mongo");
 
 const db = require("./config/mongoose");
 
